4f740c6306db09a83a61c3e5daea4295db41494b
Nicolas Raony Dantas Queiroz <163556444+NicolasRaony06@users.noreply.github.com>
2024-07-12 12:54:39 -0300

    Update views.py

--- enviar_email_django/email_send/views.py
+++ enviar_email_django/email_send/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 def send_email(request, email):
     if request.method == 'GET':
         print(type(email))
-        send_mail('Teste de Envio', 'Este é um email de teste', 'nicolas.queiroz3@aluno.ce.gov.br', [email])
+        send_mail('Teste de Envio', 'Este é um email de teste', '___', [email]) #TODO add email
         return HttpResponse('Email Enviado!')
